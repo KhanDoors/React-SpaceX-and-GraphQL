0bbe4bcb0051c40dfdc5b58e0350731d6a0954da
unknown <syyadkhan@yahoo.com>
2018-11-18 18:37:34 -0500

    final commit

---
+++ public/precache-manifest.b73d62f1d1928d30a14c08a96e87d219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37a39b35e699355acde6016e5ce69039",
+    "url": "/static/media/logo1.37a39b35.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ef7404a51077fc53b31",
+    "url": "/static/js/main.0ef7404a.chunk.js"
+  },
+  {
+    "revision": "77e091eb051f10368b03",
+    "url": "/static/js/1.77e091eb.chunk.js"
+  },
+  {
+    "revision": "0ef7404a51077fc53b31",
+    "url": "/static/css/main.ba6e277d.chunk.css"
+  },
+  {
+    "revision": "8fb20f285ef15b2d5dc943e52bf06678",
+    "url": "/index.html"
+  }
+];
